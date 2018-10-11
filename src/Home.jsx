9f1d6175343a1fb42da166479d55eeb1de3d5d2c
Kris Coulson <kriscoulson@gmail.com>
2018-10-11 14:57:56 -0700

    Add timer component

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { Link } from "@reach/router";
-import Avatar from './ui/Avatar';
 
 const Home = () => (
   <div>
     <Link to="/login">Login</Link>
-    <Avatar border="6px solid white" />
   </div>
 );
 
