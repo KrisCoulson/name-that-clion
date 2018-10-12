d90299815a3606294e581c10f2b97ef5cff21c17
Lukas Stuart-Fry <lstuartfry@gmail.com>
2018-10-11 21:17:20 -0700

    Initial commit for views/Start

--- src/ui/Inputs.jsx
+++ src/ui/Inputs.jsx
@@ -3,9 +3,13 @@ import Select from "react-select";
 import styled from "styled-components";
 
 const Input = styled.input`
-  width: 250px;
-  height: 35px;
+  min-width: 250px;
+  min-height: 35px;
   border-radius: 4px;
+  border: 1px solid #cfdde7;
+  margin: 10px 0px 15px 0px;
+  padding: 10px;
+  font-size: 14px;
 `;
 
 export const FullName = styled(Input).attrs({
@@ -17,12 +21,11 @@ export const JobTitle = styled(Input).attrs({
 })``;
 
 export const Label = styled.div`
-  font-size: 14px;
-  margin-bottom: 5px;
+  font-size: 16px;
 `;
 
 export const Location = props => (
-  <div style={{ minWidth: "250px" }}>
+  <div style={{ minWidth: "250px", margin: "10px 0px 15px 0px" }}>
     <Select
       options={props.options}
       onChange={obj => props.onChange(obj.value)}
--- src/views/start/Start.jsx
+++ src/views/start/Start.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React from "react";
+import { Link } from "@reach/router";
 
-export default function () {
+import Button from "../../ui/Button";
+
+export default function() {
   return (
-    <div>start</div>
-  )
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <div> logo goes here</div>
+      <div style={{ dispay: "flex", flexDirection: "column" }}>
+        <div>
+          <Button>Start</Button>
+        </div>
+        <div style={{ fontSize: "40px", marginTop: '30px' }}>
+          <Link to="/leaderboard">
+            <i className="fa fa-trophy" />
+          </Link>
+          <Link to="/directory">
+            <i className="fa fa-address-book" />
+          </Link>
+          <Link to="/setup">
+            <i className="fa fa-cog" />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
