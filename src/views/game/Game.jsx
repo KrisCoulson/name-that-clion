83e72d5f9ed0ba4ea655a5e1539ddeab21004db1
Kris Coulson <kriscoulson@gmail.com>
2018-10-11 17:42:50 -0700

    Try to get gh-pages

--- src/views/game/Game.jsx
+++ src/views/game/Game.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import styled from 'styled-components';
+import Timer from '../../ui/Timer';
+import GameCard from '../../ui/GameCard';
 
-export default function () {
-  return (
-    <div>game</div>
-  )
+const GameContainer = styled.div`
+  postion: relative;
+`
+const TimerPosition = styled.div`
+  position: absolute;
+  top: 10px;
+  right: 20px;
+`
+
+
+class Game extends React.Component {
+  state = {
+    gameOver: false,
+    missedNames: [],
+  }
+
+  gameOver = () => {
+    this.setState({
+      gameOver: true,
+    })
+  }
+
+  render() {
+    return (
+      <GameContainer>
+        <TimerPosition>
+          <Timer onFinish={this.gameOver}/>
+        </TimerPosition>
+        <GameCard />
+      </GameContainer>
+    )
+  }
 }
+
+export default Game;
