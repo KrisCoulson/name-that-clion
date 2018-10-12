def39d5ccf72a1b98473839b6f0d0cbed7d4ec86
Lukas Stuart-Fry <lstuartfry@gmail.com>
2018-10-11 19:30:15 -0700

    Adds react-dropzone, react-select

--- src/ui/Inputs.jsx
+++ src/ui/Inputs.jsx
@@ -1,11 +1,31 @@
+import React from "react";
+import Select from "react-select";
 import styled from "styled-components";
 
-export const JobTitle = styled.input.attrs({
+const Input = styled.input`
+  width: 250px;
+  height: 35px;
+  border-radius: 4px;
+`;
+
+export const FullName = styled(Input).attrs({
   type: "text"
 })``;
 
-export const Location = styled.select``;
-
-export const Picture  = styled.input.attrs({
-  type: 'file'
+export const JobTitle = styled(Input).attrs({
+  type: "text"
 })``;
+
+export const Label = styled.div`
+  font-size: 14px;
+  margin-bottom: 5px;
+`;
+
+export const Location = props => (
+  <div style={{ minWidth: "250px" }}>
+    <Select
+      options={props.options}
+      onChange={obj => props.onChange(obj.value)}
+    />
+  </div>
+);
--- src/views/setup/Setup.jsx
+++ src/views/setup/Setup.jsx
@@ -1,21 +1,32 @@
 import React from "react";
 import { Formik } from "formik";
+import Dropzone from "react-dropzone";
+import styled from 'styled-components';
 
-import { JobTitle, Location, Picture } from "../../ui/Inputs";
+import { FullName, JobTitle, Label, Location } from "../../ui/Inputs";
 
 const options = [
-  {
-    name: "Burnaby"
-  },
-  {
-    name: "Toronto"
-  },
-  {
-    name: "Los Angeles"
-  }
+  { value: "Burnaby", label: "Burnaby" },
+  { value: "Toronto", label: "Toronto" },
+  { value: "Los Angeles", label: "Los Angeles" }
 ];
 
+const DropzoneIcon = styled.div`
+  background-color: #EAEBEC;
+  border-radius: 100px;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  :hover {
+    cursor: pointer;
+  }
+`;
+
 class Setup extends React.Component {
+  onDrop = files => console.log("files are : ", files);
+
   onSubmit = values => {
     console.log(values);
   };
@@ -23,51 +34,62 @@ class Setup extends React.Component {
   render() {
     return (
       <Formik
-        initialValues={{
-          jobTitle: ""
-        }}
         onSubmit={this.onSubmit}
-      >
-        {props => {
-          const { values, handleChange, handleSubmit, setFieldValue } = props;
-          return (
-            <form onSubmit={handleSubmit}>
-              <div>
-                <h2>fill in your job title</h2>
-                <JobTitle
-                  placeholder="type in your job here"
-                  name="jobTitle"
-                  onChange={handleChange}
-                  value={values.jobTitle}
-                />
-              </div>
-              <div>
-                <h2>Enter your location</h2>
-                <Location
-                  name="location"
-                  onChange={handleChange}
-                  value={values.location}
-                >
-                  {options.map(option => (
-                    <option key={option.name} value={option.name}>
-                      {option.name}
-                    </option>
-                  ))}
-                </Location>
-              </div>
-              <div>
-                <h2>Upload your picture here</h2>
-                <Picture
-                  name="picture"
-                  onChange={event => {
-                    setFieldValue("file", event.currentTarget.files[0]);
-                  }}
-                />
-              </div>
-              <button type="submit">Save</button>
-            </form>
-          );
+        initialValues={{
+          fullName: "",
+          jobTitle: "",
+          location: ""
         }}
+      >
+        {({ values, handleSubmit, setFieldValue }) => (
+          <form onSubmit={handleSubmit}>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <Dropzone
+                accept="image/png"
+                onDrop={this.onDrop}
+              >
+                <DropzoneIcon>hello</DropzoneIcon>
+              </Dropzone>
+            </div>
+            <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
+              <div>
+              <Label>Enter your full name</Label>
+              <FullName
+                name="fullName"
+                onChange={({ target: { value } }) =>
+                  setFieldValue("fullName", value)
+                }
+                placeholder="type your full name here"
+                value={values.fullName}
+              />
+            </div>
+          </div>
+          <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
+            <div>
+              <Label>Enter your job title</Label>
+              <JobTitle
+                placeholder="type your job title here"
+                name="jobTitle"
+                onChange={({ target: { value } }) =>
+                  setFieldValue("jobTitle", value)
+                }
+                value={values.jobTitle}
+              />
+            </div>
+          </div>
+          <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
+            <div>
+              <Label>Enter your location</Label>
+              <Location
+                name="location"
+                onChange={value => setFieldValue("location", value)}
+                options={options}
+              />
+            </div>
+          </div>
+            <button type="submit">Save</button>
+          </form>
+        )}
       </Formik>
     );
   }
