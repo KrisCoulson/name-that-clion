b468fef90438b4318244caf9b92733da506af59a
Kris Coulson <kriscoulson@gmail.com>
2018-10-11 21:19:22 -0700

    style and almost making drag and drop work but uhhhhh :(

--- src/ui/GameCard.jsx
+++ src/ui/GameCard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Avatar from "./Avatar";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 const Possibility = styled.div`
   border-radius: 6px;
@@ -14,7 +15,7 @@ const Possibility = styled.div`
 const Text = styled.div`
   font-family: "Nunito", sans-serif;
   color: white;
-`
+`;
 
 const Heading = styled(Text)`
   font-size: 24px;
@@ -24,39 +25,132 @@ const Heading = styled(Text)`
 const Subtext = styled(Text)`
   font-size: 18px;
   font-weight: 600;
-`
+`;
 
-
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
+const Possibilities = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  flex: 1;
+`;
 
 const possibilities = [
   {
+    id: 1,
     name: "Kris Coulson",
     position: "Frontend Developer",
     location: "Los Angeles"
   },
   {
+    id: 2,
     name: "Lukas Stuart-Fry",
     position: "Frontend Developer",
     location: "Los Angeles"
   },
   {
+    id: 3,
     name: "Jeff Consing",
     position: "Designer",
     location: "Los Angeles"
   }
 ];
 
-const GameCard = () => (
-  <>
-    <Avatar border="8px solid white"/>
-    {possibilities.map(possibility => (
-      <Possibility>
-        <Heading>{possibility.name}</Heading>
-        <Subtext>{possibility.position}</Subtext>
-        <Subtext>{possibility.location}</Subtext>
-      </Possibility>
-    ))}
-  </>
-);
+class GameCard extends React.Component {
+  // state = {
+  //   dropzones: []
+  // }
+  // onDragend () => {
+  //   const { destination, source, draggableId } = result;
+  //
+  //   if (!destination) {
+  //     return;
+  //   }
+  //
+  //   if (
+  //     destination.droppableId === source.droppableId &&
+  //     destination.index === source.index
+  //   ) {
+  //     return;
+  //   }
+  //
+  //   const start = this.state.columns[source.droppableId];
+  //   const finish = this.state.columns[destination.droppableId];
+  //
+  //   if (start === finish) {
+  //     const newTaskIds = Array.from(start.taskIds);
+  //     newTaskIds.splice(source.index, 1);
+  //     newTaskIds.splice(destination.index, 0, draggableId);
+  //
+  //     const newColumn = {
+  //       ...start,
+  //       taskIds: newTaskIds,
+  //     };
+  //
+  //     const newState = {
+  //       ...this.state,
+  //       columns: {
+  //         ...this.state.columns,
+  //         [newColumn.id]: newColumn,
+  //       },
+  //     };
+  //
+  //     this.setState(newState);
+  //     return;
+  //
+  // }
+  render() {
+    return (
+      <Container>
+        <DragDropContext onDragEnd={this.onDragEnd}>
+          <Droppable droppableId="droppable-1">
+            {(droppableProvided, snapshot) => (
+              <div ref={droppableProvided.innerRef}>
+                <Draggable
+                  innerRef={droppableProvided.innerRef}
+                  draggableId="draggable-1"
+                  index={0}
+                >
+                  {(provided, snapshot) => (
+                    <Avatar
+                      innerRef={provided.innerRef}
+                      border="8px solid white"
+                      size="200px"
+                      style={{ marginTop: "80px", alignSelf: "center" }}
+                      innerRef={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    />
+                  )}
+                </Draggable>
+              </div>
+            )}
+          </Droppable>
+          <Possibilities>
+            {possibilities.map(possibility => (
+              <Droppable
+                droppableId={`dropper-${possibility.id}`}
+                key={possibility.id}
+              >
+                {(provided, snapshot) => (
+                  <Possibility innerRef={provided.innerRef}>
+                    {provided.placeholder}
+                    <Heading>{possibility.name}</Heading>
+                    <Subtext>{possibility.position}</Subtext>
+                    <Subtext>{possibility.location}</Subtext>
+                  </Possibility>
+                )}
+              </Droppable>
+            ))}
+          </Possibilities>
+        </DragDropContext>
+      </Container>
+    );
+  }
+}
 
 export default GameCard;
